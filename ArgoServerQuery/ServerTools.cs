133bb1052652c485c1b6fee66adad4c3187c95c3
Eric Woolard <premierproductions601@gmail.com>
2017-05-05 04:27:07 -0500

    Small edit

--- ArgoServerQuery/ServerTools.cs
+++ ArgoServerQuery/ServerTools.cs
@@ -66,7 +66,7 @@ namespace ArgoServerQuery
             }
             if (response == "[SM] No matching client was found.")
             {
-                return null;
+                return response;
             }
 
             string[] split = response.Split(':');
